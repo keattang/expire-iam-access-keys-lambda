d59849be592e84c7c451c57e1e4e64962784074e
Timo Ulich <ulich@users.noreply.github.com>
2022-03-17 11:36:58 +0100

    Migrate to AWS SDK v3 and enable pagination for listUsers

--- src/aws/deleteAccessKey.js
+++ src/aws/deleteAccessKey.js
@@ -1,13 +1,14 @@
+const { DeleteAccessKeyCommand } = require('@aws-sdk/client-iam');
 const { iam } = require('./services');
 const { DRY_RUN } = require('../config');
 
 const deleteAccessKey = async (user, key) => {
-    const resp = await iam
-        .deleteAccessKey({
+    const resp = await iam.send(
+        new DeleteAccessKeyCommand({
             UserName: user.UserName,
             AccessKeyId: key.AccessKeyId,
         })
-        .promise();
+    );
 
     if (DRY_RUN) return Promise.resolve();
     return resp;
--- src/aws/getAccessKeys.js
+++ src/aws/getAccessKeys.js
@@ -1,7 +1,12 @@
+const { ListAccessKeysCommand } = require('@aws-sdk/client-iam');
 const { iam } = require('./services');
 
 const getAccessKeys = async (user) => {
-    const resp = await iam.listAccessKeys({ UserName: user.UserName }).promise();
+    const resp = await iam.send(
+        new ListAccessKeysCommand({
+            UserName: user.UserName,
+        })
+    );
     return resp.AccessKeyMetadata;
 };
 
